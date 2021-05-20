db6efe2809aa4efbf25707a758d08dae6ce6dc31
qq63949774 <84216269+qq63949774@users.noreply.github.com>
2021-05-20 13:08:54 +0800

    day five
    添加了子弹的类， 来控制子弹的飞行。

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
@@ -16,8 +17,10 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         # 设置背景色。
         self.bg_color = (230, 230, 230)
+
 
     def run_game(self):
         """开始游戏的主循环
@@ -29,11 +32,14 @@ class AlienInvasion:
             self._check_events()
             self._update_screen()
             self.ship.update()
+            self.bullets.update()
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕。"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
@@ -60,6 +66,8 @@ class AlienInvasion:
             self.ship.moving_up = True
         elif event.key == pygame.K_DOWN:
             self.ship.moving_down = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     def _check_keyup_events(self, event):
         """响应松开"""
         if event.key == pygame.K_RIGHT:
@@ -70,6 +78,12 @@ class AlienInvasion:
             self.ship.moving_up = False
         if event.key == pygame.K_DOWN:
             self.ship.moving_down = False
+    def _fire_bullet(self):
+        """创建一个子弹，并将其加入编组bullets中。"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+
 
 
 if __name__ == '__main__':
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -9,5 +9,10 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        # 飞船设置
+        # 飞船设置。
         self.ship_speed = 3.5
+        # 子弹设置。
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
