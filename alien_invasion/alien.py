a39cd70d4fe64c5f367fddacb65ebb15d0d8397a
qq63949774 <84216269+qq63949774@users.noreply.github.com>
2021-06-02 00:35:03 +0800

    Add files via upload

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -6,6 +6,7 @@ class Alien(Sprite):
         """初始化外星人并设置其初始位置"""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         # 加载外星人图像并设置其rect属性。
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
@@ -14,3 +15,8 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
         # 存储外星人的精确水平位置。
         self.x = float(self.rect.x)
+
+    def update(self):
+        """向右移动外星人。 """
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -17,3 +17,5 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 5
+        # 外星人设置
+        self.alien_speed = 1.0
