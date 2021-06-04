eff79e9698af625e00bf42df69b45fbd0170d4a8
qq63949774 <84216269+qq63949774@users.noreply.github.com>
2021-06-04 12:58:05 +0800

    day 11
    给游戏添加了递增的难度， 添加了计分板。接下来需要设计外星人被消灭时更新得分。

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -6,6 +6,8 @@ from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import  Alien
+from button import Button
+from scoreboard import Scoreboard
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
@@ -14,17 +16,21 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+        self.screen = pygame.display.set_mode((2560, 1200))
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Invasion")
         # 创建一个用于存储游戏统计信息的实例.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)  # 创建记分牌。
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
         self._create_fleet()
+        # 创建 Play按钮。
+        self.play_button = Button(self, "Play")
 
 
 
@@ -53,6 +59,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+        self.sb.show_score()  # 显示得分
+        # 如果游戏处于非活动状态， 就绘制Play按钮。
+        if not self.stats.game_active:
+            self.play_button.draw_button()
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
@@ -65,6 +75,9 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
 
 
     def _check_keydown_events(self, event):
@@ -116,6 +129,7 @@ class AlienInvasion:
             # 删除现有的子弹并新建一群外星人
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
     def _create_fleet(self):
         """创建外星人群。"""
         # 创建一个外星人并计算一行可容纳多少个外星人。
@@ -192,6 +206,28 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+
+            pygame.mouse.set_visible(True)
+
+    def _check_play_button(self, mouse_pos):
+        """在玩家单击Play按钮时开始新游戏。"""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # 重置游戏设置。
+            self.settings.initialize_dynamic_settings()
+            # 重置游戏统计信息。
+            self.stats.reset_stats()
+            self.stats.game_active = True
+            # 清空余下的外星人和子弹。
+            self.aliens.empty()
+            self.bullets.empty()
+            # 创建一群新的外星人并让飞船居中。
+            self._create_fleet()
+            self.ship.center_ship()
+            # 隐藏鼠标的光标。
+            pygame.mouse.set_visible(False)
+
+
 
 
 if __name__ == '__main__':
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -8,10 +8,10 @@ class Settings:
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (200, 200, 200)
+        self.bg_color = (230, 230, 230)
         # 飞船设置。
         self.ship_speed = 8.5
-        self.ship_limit = 3
+        self.ship_limit = 1
         # 子弹设置。
         self.bullet_speed = 3.0
         self.bullet_width = 3
@@ -19,7 +19,21 @@ class Settings:
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 5
         # 外星人设置
-        self.alien_speed = 10
         self.fleet_drop_speed = 10
-        # fleet_direction 为1表示向右移， 为-1表示向左移。
+        # 加快游戏节奏。
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行而变化的设置。 """
+        self.ship_speed = 8.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+        # fleet_direction为1表示向右，为-1表示向左。
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """提高速度设置。"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
