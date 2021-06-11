a9122175b673ad1fd11d1021b3c531a4eb39bab0
qq63949774 <84216269+qq63949774@users.noreply.github.com>
2021-06-11 12:44:02 +0800

    数据可视化 6.11
    学习到 15.3.1 创建RandomWalk

--- chapter_15/可视化练习.py
+++ chapter_15/可视化练习.py
@@ -3,17 +3,16 @@ from pylab import mpl
 plt.style.use('seaborn')
 mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
 mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
-x_values = range(1, 1001)
-y_values = [x**2 for x in x_values]
+x_values = range(1, 5000)
+y_values = [x**3 for x in x_values]
 fig, ax = plt.subplots()
 ax.scatter(x_values, y_values, c = y_values, cmap=plt.cm.Blues, s=10)
 # 设置图表标题并给坐标轴加上标签。
-ax.set_title("平方数", fontsize = 24)
-ax.set_xlabel("值", fontsize = 14)
-ax.set_ylabel("值的平方", fontsize = 14)
+ax.set_title('立方数', fontsize = 25)
+ax.set_xlabel('值', fontsize = 25)
+ax.set_ylabel('值的立方', fontsize = 15)
 # 设置刻度标记的大小。
 ax.tick_params(axis = 'both', which = 'major', labelsize= 14)
 # 设置每个坐标轴的取值范围。
-ax.axis([0, 1100, 0, 1100000])
+ax.axis([0, 5000, 0, 100000000000])
 plt.show()
-plt.savefig('squares_plot.png', bbox_inches='tight')
